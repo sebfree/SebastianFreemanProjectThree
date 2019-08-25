5cbdcf0359b36ad8584376e62f3541873f5a7c48
Seb <free.sebastian@gmail.com>
2019-08-25 17:27:42 -0400

    finalfinal

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-// how to organize code 
 
-
-//  •	The app will ask the user 3 questions 
-// •	The app will save the user’s answers
-// •	The app will use stored user answers to determine the user’s wrestler name 
-// •	The app will display the user’s fictitious wrestler name 
-// •	1 array, 3 names
-// •	Tabulate score based on user answers (e.g. user gets 2 points for true answer, and so on)
-
-// const myApp = {};
-// myApp.wrestlerNames = ["The Jabrone", "Holay Molay", "Masked Kitten"];
-// myApp.score = myApp.score + (radioValue) + (radioValue1) + (radioValue2);
-
-
-// myApp.init = function () {
-    // myApp.getWrestlerName();
-    // myApp.assignName();
-    // myApp.totalScore();
-// }
-
-// document ready - load all jquery content on page
 $(function() {
 
 
